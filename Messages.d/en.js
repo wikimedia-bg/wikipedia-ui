e2d408c0c754d9706648c3e525e2627490d95555
Borislav <b.manolov@gmail.com>
2008-06-03 16:49:02 +0000

    javascript messages in English

---
+++ Messages.d/en.js
@@ -0,0 +1,34 @@
+_lang_messages["en"] = {
+// Projects
+"wikipedia": "Wikipedia",
+"wiktionary": "Wiktionary",
+"wikiquote": "Wikiquote",
+"wikibooks": "Wikibooks",
+"wikisource": "Wikisource",
+"wikinews": "Wikinews",
+"wikiversity": "Wikiversity",
+"wikispecies": "Wikispecies",
+"commons": "Wikimedia Commons",
+
+// Edit tools
+"et-addchar": "Add character “$1”",
+"et-addpref": "Add ",
+"et-ddmenutitle": "From this menu you can insert an empty template",
+"et-ajaxerror": "Connection error: $1 $2\nPage: $3\nAddress: $4",
+"et-tplloading": "Loading template…",
+
+// Featured article marker
+"fa-linktitle" : "This article in other language is featured.",
+
+// Transclusion tool
+"ta-emptyfield" : "You have not entered any subpage name.",
+"ta-summary" : "Transcluding [[$1]]",
+"ta-bpsummary" : "New comment: [[$1]]",
+
+// Toolbox add-ons
+"tb-subpages": "Subpages",
+"tb-subpages-settings": "Show a Subpages link in the toolbox:",
+"tb-inother": "In other projects",
+
+"":""
+};
