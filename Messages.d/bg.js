4bb418231e233378e55b6af84702fdf0c7305f02
Borislav <b.manolov@gmail.com>
2008-06-03 16:48:35 +0000

    джаваскриптови съобщения на български

---
+++ Messages.d/bg.js
@@ -0,0 +1,34 @@
+_lang_messages["bg"] = {
+// Projects
+"wikipedia": "Уикипедия",
+"wiktionary": "Уикиречник",
+"wikiquote": "Уикицитат",
+"wikibooks": "Уикикниги",
+"wikisource": "Уикиизточник",
+"wikinews": "Уикиновини",
+"wikiversity": "Уикиверситет",
+"wikispecies": "Уикивидове",
+"commons": "Общомедия",
+
+// Edit tools
+"et-addchar": "Вмъкване на знака „$1“",
+"et-addpref": "Вмъкване на ",
+"et-ddmenutitle": "Оттук можете да вмъкнете празен шаблон",
+"et-ajaxerror": "Неуспешна връзка: $1 $2\nСтраница: $3\nАдрес: $4",
+"et-tplloading": "Шаблонът се зарежда…",
+
+// Featured article marker
+"fa-linktitle" : "Тази статия на друг език е избрана.",
+
+// Transclusion tool
+"ta-emptyfield" : "Не сте въвели име за подстраницата.",
+"ta-summary" : "Вграждане на [[$1]]",
+"ta-bpsummary" : "Нова тема: [[$1]]",
+
+// Toolbox add-ons
+"tb-subpages": "Подстраници",
+"tb-subpages-settings": "Показване на препратка към подстраниците в кутията с инструменти:",
+"tb-inother": "В други проекти",
+
+"":""
+};
