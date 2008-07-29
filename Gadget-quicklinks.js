ef3fb9205101fa3b929bdae40cd5ba406b6f6641
Borislav <b.manolov@gmail.com>
2008-07-29 18:57:30 +0000

    леко преформяне, скриване на връзките при щракане извън контейнера

--- Gadget-quicklinks.js
+++ Gadget-quicklinks.js
@@ -3,17 +3,15 @@
 	@uses Mwbot ([[Потребител:Borislav/mwbot.js]])
 	@uses Creator
 
-	License: GNU GPL 2 or later / GNU FDL
+	License: Public domain
 	Author: Borislav Manolov
 */
 
 importScript('Потребител:Borislav/mwbot.js');
 
 var showQuickLink = (function() {
-	var quickPage = 'User:' + wgUserName +'/Бързи връзки';
-	var quickLink = null;
-	var quickLinks = null;
-	var editLink = null;
+	var quickPage = 'Бързи връзки';
+	var page = 'User:' + wgUserName +'/' + quickPage;
 	var parents = {
 		// skin : parent element ID
 		'standard' : 'searchform',
@@ -26,35 +24,67 @@ var showQuickLink = (function() {
 		'modern' : 'pt-userpage'
 	};
 	var plus = '+', minus = '&minus;', wait = '…';
+	var link, container = null;
 	var loaded = false;
 
-	var showContainer = function(content) {
-		if ( quickLinks === null ) {
-			quickLinks = Creator.createElement('div', {
+	function showContainer(content) {
+		if ( container === null ) {
+			container = createContainer();
+			document.getElementsByTagName('body')[0].appendChild(container.e);
+		}
+		container.set('<div class="editsection" style="float:right">[<a href="'
+			+ Creator.createInternUrl(page, 'edit')
+			+ '" title="Редактиране на страницата с бързите връзки">'
+			+ 'редактиране</a>]</div>');
+		container.add( content.indexOf('emptypage') != -1
+			? '<em>Страницата ви с бързи връзки е празна.</em>'
+			: content);
+		link.innerHTML = minus;
+	}
+
+	function createContainer() {
+		var c = {
+			e : Creator.createElement('div', {
 				'id' : 'myquicklinks',
 				'class' : 'messagebox',
 				'style' : 'position: absolute; top: 3em; left: 3em; z-index: 10; overflow: auto; width: 90%; padding: 1em'
-			});
-			document.getElementsByTagName('body')[0].appendChild(quickLinks);
-		}
-		quickLinks.innerHTML = '<div class="editsection" style="float:right">[<a href="'
-			+ Creator.createInternUrl(quickPage, 'edit')
-			+ '" title="Редактиране на страницата с бързите връзки">'
-			+ 'редактиране</a>]</div>';
-		quickLinks.innerHTML += content.indexOf('emptypage') != -1
-			? '<em>Страницата ви с бързи връзки е празна.</em>'
-			: content;
-		quickLink.innerHTML = minus;
-	};
+			}),
+			show : function() {
+				this.e.style.display = '';
+				link.innerHTML = minus;
+			},
+			hide : function() {
+				this.e.style.display = 'none';
+				link.innerHTML = plus;
+			},
+			toggle : function() {
+				this.isHidden() ? this.show() : this.hide();
+				this.isJustToggled = true;
+			},
+			isJustToggled : false,
+			set : function(content) {
+				this.e.innerHTML = content;
+			},
+			add : function(content) {
+				this.e.innerHTML += content;
+			},
+			isHidden : function() {
+				return this.e.style.display == 'none';
+			}
+		};
+		c.e.onclick = function(event) { event.stopPropagation(); };
+		return c;
+	}
 
-	var showFullContainer = function(bot) {
+	function showFullContainer(bot) {
 		showContainer(bot.page_content);
 		loaded = true;
-	};
+	}
 
-	var showEmptyContainer = function(bot) {
+	function showEmptyContainer(bot) {
 		showContainer('Бързите връзки не можаха да бъдат заредени.');
-	};
+	}
+
 
 	return (function() {
 		if ( typeof parents[skin] == "undefined" ) {
@@ -65,32 +95,30 @@ var showQuickLink = (function() {
 			alert('В текущия документ не съществува елемент с ID „'+p+'“. Контролерът на бързите връзки не може да бъде зареден.');
 			return;
 		}
-
-		quickLink = Creator.createAnchor( Creator.createInternUrl(quickPage),
+		link = Creator.createAnchor( Creator.createInternUrl(page),
 			plus, 'Показване на бързите връзки');
-		quickLink.onclick = function() {
+		link.onclick = function(event) {
+			event.stopPropagation();
 			if ( loaded ) {
-				if ( quickLinks.style.display == 'none' ) {
-					quickLinks.style.display = '';
-					quickLink.innerHTML = minus;
-				} else {
-					quickLinks.style.display = 'none';
-					quickLink.innerHTML = plus;
-				}
+				container.toggle();
 				return false;
 			}
-			quickLink.innerHTML = wait;
+			link.innerHTML = wait;
 			var bot = new Mwbot();
 			bot.register_hook('on_fetch', showFullContainer);
 			bot.register_hook('on_fetch_fail', showEmptyContainer);
-			bot.fetchHtml(quickPage);
+			bot.fetchHtml(page);
 			return false;
 		};
 
 		var c = Creator.createElement('span',
 			{'style' : 'z-index: 11'},
-			[' (', quickLink, ')']);
+			[' (', link, ')']);
 		p.appendChild(c);
+		document.getElementsByTagName("body")[0].onclick = function() {
+			// hide container by clicking anywhere in the document
+			container.hide();
+		};
 	});
 
 })();
