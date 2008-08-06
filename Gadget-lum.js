6570d51244c63486567bd7e39a0db6d751a3aad8
Borislav <b.manolov@gmail.com>
2008-08-06 17:30:07 +0000

    хайде малко по-умно сменяне на заглавието

--- Gadget-lum.js
+++ Gadget-lum.js
@@ -13,8 +13,12 @@ addOnloadHook(function() {
 	var imgs = document.getElementsByTagName("img");
 	for (var i = 0, img; img = imgs[i]; i++) {
 		if ( img.src.indexOf(sharedRepoKey) == -1 ) {
-			// change anchor title — browsers show this instead of img.alt/title
-			img.parentNode.title = prefix + img.parentNode.title;
+			if ( img.parentNode.nodeName.toLowerCase() == "a" ) {
+				// change anchor title — browsers show this instead of img.alt/title
+				img.parentNode.title = prefix + img.parentNode.title;
+			} else {
+				img.title = prefix + img.title;
+			}
 		}
 	}
 });
