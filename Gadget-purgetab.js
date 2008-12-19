e93624d2f15911668f2b24ef1bc54069910a0e45
Петър Петров <>
2008-12-19 10:02:38 +0000

    взето от [[m:MediaWiki:Gadget-purgetab.js]]

---
+++ Gadget-purgetab.js
@@ -0,0 +1,9 @@
+/* Purge caption button */
+addOnloadHook(function () {
+   var hist; var url;
+   if (!(hist = document.getElementById('ca-history') )) return;
+   if (!(url = hist.getElementsByTagName('a')[0] )) return;
+   if (!(url = url.href )) return;
+   addPortletLink('p-cactions', url.replace(/([?&]action=)history([&#]|$)/, '$1purge$2'),
+                  '*', 'ca-purge', 'Изчистване на складираното копие на страницата', '*');
+});
