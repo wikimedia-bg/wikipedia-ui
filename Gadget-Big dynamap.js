91cc37b3a3cdc8eb717b851728b25964ce0a5dd2
Borislav <b.manolov@gmail.com>
2009-01-09 10:06:14 +0000

    не работеше при повече от една карти на страница

--- Gadget-Big dynamap.js
+++ Gadget-Big dynamap.js
@@ -7,39 +7,30 @@ importScript("МедияУики:JQuery.js");
 	@uses jQuery
 */
 addOnloadHook(function() {
-	var _dynamapDone = false;
 	$("div.dynamap").each(function(){
-		if ( _dynamapDone ) {
-			return false;
-		}
-
 		var newMap = $(this).clone();
 		var newImg = $("img", newMap).eq(0); // the map image
 		var oldWidth = newImg[0].width;
 		var newWidth = 600;
-		newImg[0].src = newImg[0].src.replace(/\d+px/, newWidth + "px");
+		newImg[0].src = newImg[0].src.replace(/\d+(px)/, newWidth + "$1");
 		newImg[0].width = newWidth;
-		newImg[0].height *= (newWidth / oldWidth);
+		newImg[0].height *= newWidth / oldWidth;
 
 		var mapOffset = $(this).offset();
 		newMap.css({
 			"z-index" : 10,
 			position  : "absolute",
-			display   : "none",
-			width     : newWidth + "px",
 			top       : mapOffset.top,
-			left      : mapOffset.left - ( newWidth - oldWidth ) + 5
+			left      : mapOffset.left - ( newWidth - oldWidth ) + 5,
+			width     : newWidth + "px"
 		}).click(function(){
 			$(this).hide();
 			return false;
-		}).appendTo(document.body);
+		}).hide().appendTo(document.body);
 
 		$("a", this).click(function(){
 			newMap.show();
-			return false;
+			return false; // tell the browser to not follow the image link
 		});
-
-		_dynamapDone = true;
-		return false;
 	});
 });
