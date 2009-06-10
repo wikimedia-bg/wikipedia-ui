cb0cf655ff0b11644bef6640af0ffab8332b5f62
Borislav <b.manolov@gmail.com>
2009-06-10 13:12:23 +0000

    пренасочване към Промените, ако wgxQuickPatrolLoadRc = true

--- Gadget-Quick patrol.js
+++ Gadget-Quick patrol.js
@@ -6,11 +6,16 @@ if ( window.importScript && typeof jQuery == "undefined" ) {
 	Ajaxify patrol links.
 	@uses jQuery
 */
+var wgxQuickPatrolLoadRc = false;
+
 addOnloadHook(function(){
 	$(".patrollink a").click(function(){
 		var $link = $(this).addClass("loading");
 		$.post(this.href, function(data){
 			$link.after("Версията беше отбелязана като проверена.").remove();
+			if ( wgxQuickPatrolLoadRc ) {
+				location.href = Creator.createInternUrl("Специални:Последни промени");
+			}
 		});
 		return false;
 	});
