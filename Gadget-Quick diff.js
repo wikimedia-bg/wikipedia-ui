aaaa09ec29debc9e488065641458088338af9584
Borislav <b.manolov@gmail.com>
2009-06-23 20:54:05 +0000

    Enhance recent changes diff links - view diffs directly in the recent changes page

---
+++ Gadget-Quick diff.js
@@ -0,0 +1,63 @@
+/**
+	Enhance recent changes diff links.
+	Author: Borislav Manolov
+	License: Public domain
+*/
+
+if ( window.importScript && ! window.jQuery ) {
+	importScript("МедияУики:Gadget-jQuery.js");
+}
+
+
+/**
+	@uses jQuery
+*/
+var QuickDiff = {
+
+	enable: function()
+	{
+		$("a[href*=diff]").click(function(){
+			var $link = $(this).addClass("working");
+			$.get(this.href + "&diffonly=1&action=render", function(data){
+				QuickDiff.viewDiff(data, $link.offset().top);
+				$link.removeClass("working");
+			});
+			return false;
+		});
+	},
+	
+	viewDiff: function(content, top)
+	{
+		this.prepareViewer();
+		this.viewWindow.css("top", top).html(content).show();
+	},
+	
+	viewWindow: null,
+
+	prepareViewer: function()
+	{
+		if ( null !== this.viewWindow ) {
+			return;
+		}
+
+		importStylesheetURI(stylepath + "/common/diff.css");
+
+		this.viewWindow = $("<div/>")
+			.css({
+				position: "absolute",
+				top: 0,
+				border: "medium outset silver"
+			})
+			.dblclick(function(){
+				$(this).hide();
+			})
+			.appendTo("#content");
+	}
+};
+
+// prepare for fight
+addOnloadHook(function(){
+	if ( /^(Recentchanges|Watchlist)/.test(wgCanonicalSpecialPageName) ) {
+		QuickDiff.enable();
+	}
+});
