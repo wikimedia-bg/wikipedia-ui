ed9565e69198ca6bd34a36c9cc8d411065a1b692
Borislav <b.manolov@gmail.com>
2009-11-19 17:33:30 +0000

    ползване на променливата jQuery вместо $; UsabilityInitiative внася jQuery, но без $

--- Gadget-Big dynamap.js
+++ Gadget-Big dynamap.js
@@ -1,24 +1,24 @@
-if ( window.importScript && typeof jQuery == "undefined" ) {
+if ( window.importScript && ! window.jQuery ) {
 	importScript("МедияУики:Gadget-jQuery.js");
 }
 
-/** 
+/**
 	Create a bigger map for each [[Шаблон:ПК|dynamic map]].
 	The new map should be shown on a mouse click.
 	The old image links are disabled.
 	@uses jQuery
 */
 addOnloadHook(function() {
-	$("div.dynamap").each(function(){
-		var newMap = $(this).clone();
-		var newImg = $("img", newMap).eq(0); // the map image
+	jQuery("div.dynamap").each(function(){
+		var newMap = jQuery(this).clone();
+		var newImg = jQuery("img", newMap).eq(0); // the map image
 		var oldWidth = newImg[0].width;
 		var newWidth = 600;
 		newImg[0].src = newImg[0].src.replace(/\d+(px)/, newWidth + "$1");
 		newImg[0].width = newWidth;
 		newImg[0].height *= newWidth / oldWidth;
 
-		var mapOffset = $(this).offset();
+		var mapOffset = jQuery(this).offset();
 		newMap.css({
 			"z-index" : 10,
 			position  : "absolute",
@@ -26,11 +26,11 @@ addOnloadHook(function() {
 			left      : mapOffset.left - ( newWidth - oldWidth ) + 5,
 			width     : newWidth + "px"
 		}).click(function(){
-			$(this).hide();
+			jQuery(this).hide();
 			return false;
 		}).hide().appendTo(document.body);
 
-		$("a", this).click(function(){
+		jQuery("a", this).click(function(){
 			newMap.show();
 			return false; // tell the browser to not follow the image link
 		});
