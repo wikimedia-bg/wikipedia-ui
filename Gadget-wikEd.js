f0518eefea836999c4814849215dfd24ed9876c9
Borislav <b.manolov@gmail.com>
2010-01-10 17:27:43 +0000

    вмъкване от [[en:User:Cacycle/wikEd.js]]

--- Gadget-wikEd.js
+++ Gadget-wikEd.js
@@ -1,1 +1,7 @@
-importOuterScript("MediaWiki:Gadget-wikEd.js", "en");
+// Imports [[en:User:Cacycle/wikEd.js]]
+// wikEd is a full-featured in-browser editor for Wikipedia, see [[en:User:Cacycle/wikEd]]
+
+// disable loading for IE, not needed, but might save a few milliseconds
+if (navigator.appName != 'Microsoft Internet Explorer') {
+	importOuterScript("User:Cacycle/wikEd.js", "en");
+}
