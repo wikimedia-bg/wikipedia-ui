6269c1c78f84773d1fc7289fd0a722d425afa700
Borislav <b.manolov@gmail.com>
2010-01-10 19:17:22 +0000

    да работи с всички езици

--- Gadget-externalsearch.js
+++ Gadget-externalsearch.js
@@ -1,1 +1,4 @@
 importOuterScript("MediaWiki:Gadget-externalsearch.js", "en");
+if (wgCanonicalNamespace == "Special" && wgCanonicalSpecialPageName == "Search") {
+	addOnloadHook(SpecialSearchEnhanced);
+}
