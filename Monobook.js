a5af9ae89613e8b97fa5c5fef8048d99f193a5d8
Borislav <b.manolov@gmail.com>
2013-11-09 10:15:43 +0000

    от common.js

---
+++ Monobook.js
@@ -0,0 +1,33 @@
+/**
+	Add a start new page input box to the sidebar.
+	Author: Borislav Manolov
+	License: Public domain
+*/
+gLang.addMessages({
+	"p-createnew" : "Нова страница",
+	"action-create" : "Създаване"
+}, "bg");
+gLang.addMessages({
+	"p-createnew" : "Create New Page",
+	"action-create" : "Create"
+}, "en");
+
+addOnloadHook(function(){
+	var psearch = document.getElementById("p-search");
+	if ( ! psearch) {
+		return;
+	}
+
+	var header = Creator.createElement('h5', {}, gLang.msg("p-createnew"));
+	var form = Creator.createElement('form', {
+		action : wgScript, id: "createform"
+	}, Creator.createElement("div", {}, [
+		Creator.createHiddenField("action", "edit"),
+		Creator.createElement("input", {type: "text", name: "title", id: "createPageInput"}),
+		Creator.createElement("input", {type: "submit", "class": "searchButton", value: gLang.msg("action-create")})
+	]));
+	var box = Creator.createElement('div', {'class' : 'pBody'}, form);
+	var portlet = Creator.createElement( 'div',
+		{'class' : 'portlet', 'id' : 'p-create'}, [header, box] );
+	psearch.parentNode.insertBefore(portlet, psearch.nextSibling);
+});
