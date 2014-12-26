9118a788af098bf44d06add8fb8b353c72b7ab54
Borislav <b.manolov@gmail.com>
2014-12-26 23:03:21 +0000

    do patrolling by revid instead of rcid; use mw.Api for patrol token; other rewrites

--- Gadget-Quick patrol.js
+++ Gadget-Quick patrol.js
@@ -18,45 +18,41 @@ gLang.addMessages({
 /**
  * Ajaxify patrol links
  */
-var QuickPattroler = {
+window.QuickPatroller = {
 	linkClassWorking: "working",
 
-	enable: function()
-	{
-		jQuery("body").on("click", ".patrollink a", function(){
-			QuickPattroler.executePatrol(this);
+	enable: function() {
+		$("body").on("click", ".patrollink a", function(){
+			QuickPatroller.executePatrol(this);
 			return false;
 		});
 	},
 
-	executePatrol: function(link)
-	{
-		var $link = jQuery(link).addClass( this.linkClassWorking );
-		jQuery.post(link.href, function(data){
-			$link.replaceWith( gLang.msg("markedaspatrolledtext1") );
-			QuickPattroler.gotoRcIfWanted();
+	executePatrol: function(link) {
+		var $link = $(link).addClass( this.linkClassWorking );
+		$.post(link.href, function(data){
+			$link.replaceWith(gLang.msg("markedaspatrolledtext1"));
+			QuickPatroller.gotoRcIfWanted();
 		});
 	},
 
-	gotoRcIfWanted: function()
-	{
-		if ( window.wgxQuickPatrolLoadRc && wgxQuickPatrolLoadRc ) {
-			location.href = mw.util.getUrl( gLang.msg("recentchangespage") );
+	gotoRcIfWanted: function() {
+		if (window.wgxQuickPatrolLoadRc && wgxQuickPatrolLoadRc) {
+			location.href = mw.util.getUrl(gLang.msg("recentchangespage"));
 		}
 	}
 };
 
 /**
- * Enable bunch patrolling when multiple edits are reviewed at once
+ * Enable bulk patrolling when multiple edits are reviewed at once
  * thru the enhanced recent changes
  */
-var BunchPatroller = {
+window.BulkPatroller = {
 
-	apiPath: wgScriptPath + "/api.php",
-	rcidsParam: "rcids",
+	revidsParam: "patrol-revids",
 	diffsParam: "diffs",
 	paramDelim: ",",
-	bunchDiffLinkClass: "bunch-duff-link",
+	bulkDiffLinkClass: "bulk-duff-link",
 	diffLinkClass: "duff-link",
 	diffLinkClassDone: "done",
 	diffLinkClassNotDone: "not-done",
@@ -69,265 +65,192 @@ var BunchPatroller = {
 	errors: [],
 
 	/** Works on Special:Recentchanges */
-	makeBunchDiffsPatrollable: function()
-	{
-		jQuery(".mw-collapsible").each(function(){
-			BunchPatroller.makeBunchDiffPatrollable(this);
+	makeBulkDiffsPatrollable: function() {
+		$(".mw-collapsible.mw-enhanced-rc").each(function() {
+			if ($(this).find(".unpatrolled").length) {
+				BulkPatroller.makeBulkDiffPatrollable(this);
+			}
 		});
 	},
 
-	makeBunchDiffPatrollable: function(changeslist)
-	{
-		var $rcidLinks = jQuery('tr:gt(0) a[href*="rcid"]', changeslist);
-		var rcids = BunchPatroller.getJoinedValueFromHrefs($rcidLinks, /rcid=(\d+)/);
-		if ( "" !== rcids ) {
-			var diffs = BunchPatroller.getJoinedValueFromHrefs($rcidLinks, /diff=(\d+)/, /oldid=(\d+)/);
-			this.enhanceBunchDiffLink(jQuery("tr:eq(0)", changeslist), rcids, diffs);
-		}
+	makeBulkDiffPatrollable: function(changeslist) {
+		var $timeLinks = $(".unpatrolled", changeslist).closest("tr").find(".mw-enhanced-rc-time a");
+		var revids = this.getJoinedValueFromHrefs($timeLinks, /oldid=(\d+)/);
+		this.enhanceBulkDiffLink($("tr:first", changeslist), revids);
 	},
 
-	getJoinedValueFromHrefs: function($links, regexp, regexpAlt)
-	{
+	getJoinedValueFromHrefs: function($links, regexp, regexpAlt) {
 		return $links.map(function(){
-			var m = jQuery(this).attr("href").match(regexp);
+			var m = $(this).attr("href").match(regexp);
 			if ( null === m && typeof regexpAlt == "object" ) {
 				// test the fallback regexp
-				m = jQuery(this).attr("href").match(regexpAlt);
+				m = $(this).attr("href").match(regexpAlt);
 			}
 			return null === m ? 0 : m[1];
 		}).get().join(this.paramDelim);
 	},
 
 	/**
-		Add extra parameters to the href attribute of the bunch diff link.
-		If there is no diff link (by new pages) one is created.
-	*/
-	enhanceBunchDiffLink: function($holder, rcids, diffs)
-	{
-		var extraParams = "&" + BunchPatroller.rcidsParam + "=" + rcids
-			+ "&" + BunchPatroller.diffsParam + "=" + diffs;
+	 * Add extra parameters to the href attribute of the bulk diff link.
+	 * If there is no diff link (by new pages) one is created.
+	 */
+	enhanceBulkDiffLink: function($holder, revids) {
+		var extraParams = "&" + this.revidsParam + "=" + revids;
 
-		var $link = jQuery('a[href*="diff"]', $holder);
-		if ( $link.length ) {
+		var $link = $('a[href*="diff"]', $holder);
+		if ($link.length) {
 			$link.attr("href", function(){
 				return this.href + extraParams;
-			}).addClass( BunchPatroller.bunchDiffLinkClass );
+			}).addClass(this.bulkDiffLinkClass);
 		} else {
-			this.addBunchDiffLinkTo(
-				jQuery("td:eq(1)", $holder), // second table cell
+			this.addBulkDiffLinkTo(
+				$("td:eq(1)", $holder), // second table cell
 				diffs.split(this.paramDelim).shift(), // first id
 				extraParams
 			);
 		}
 	},
 
-	addBunchDiffLinkTo: function($holder, diff, extraParams)
-	{
+	addBulkDiffLinkTo: function($holder, diff, extraParams) {
 		$holder.html( $holder.html().replace(
 			new RegExp( gLang.msg("nchanges") ),
-			'<a href="' + wgScript + "?diff=" + diff + "&oldid=" + diff + extraParams
-				+ '" class="' + BunchPatroller.bunchDiffLinkClass + '">$&</a>') );
+			'<a href="' + mw.config.get('wgScript') + "?diff=" + diff + "&oldid=" + diff + extraParams
+				+ '" class="' + this.bulkDiffLinkClass + '">$&</a>') );
 	},
 
 	/** Works on diff pages */
-	enable: function(url)
-	{
-		$bunchPatrolLinkHolder = jQuery("#mw-diff-ntitle4");
-		if ( 0 == $bunchPatrolLinkHolder.length ) {
+	enable: function(url) {
+		$bulkPatrolLinkHolder = $("#mw-diff-ntitle4");
+		if ( 0 == $bulkPatrolLinkHolder.length ) {
 			return; // not a diff page, get out of here
 		}
 
-		var rcidsRaw = mw.util.getParamValue(this.rcidsParam, url);
-		if ( ! rcidsRaw ) {
-			return; // no rcids to patrol
+		var revidsRaw = mw.util.getParamValue(this.revidsParam, url);
+		if ( ! revidsRaw ) {
+			return; // no revids to patrol
 		}
 
-		var rcids = rcidsRaw.split(this.paramDelim);
-		this.addPatrolLinkTo($bunchPatrolLinkHolder, rcids);
+		var revids = revidsRaw.split(this.paramDelim);
+		this.addPatrolLinkTo($bulkPatrolLinkHolder, revids);
 
-		var diffs = mw.util.getParamValue(this.diffsParam, url).split(this.paramDelim);
-		this.addDiffLinksTo($bunchPatrolLinkHolder, rcids, diffs);
+		this.addDiffLinksTo($bulkPatrolLinkHolder, revids);
 
-		this.addShowAllDiffsLinkTo($bunchPatrolLinkHolder);
+		this.addShowAllDiffsLinkTo($bulkPatrolLinkHolder);
 	},
 
-	addPatrolLinkTo: function($holder, rcids)
-	{
-		if ( $holder.children().length ) {
+	addPatrolLinkTo: function($holder, revids) {
+		if ($holder.children().length) {
 			$holder.append("<br/>");
 		}
-		jQuery('<a href="#executePatrol"/>')
-			.text( rcids.length == 1
+		$('<a href="#executePatrol"/>')
+			.text( revids.length == 1
 				? gLang.msg("markaspatrolledtext1")
-				: gLang.msg("markaspatrolledtext", rcids.length) )
-			.click(function(){
-				jQuery(this).addClass( QuickPattroler.linkClassWorking );
-				BunchPatroller.executePatrol(rcids, this);
+				: gLang.msg("markaspatrolledtext", revids.length) )
+			.click(function() {
+				$(this).addClass(QuickPatroller.linkClassWorking);
+				BulkPatroller.executePatrol(revids, this);
 				return false;
 			})
 			.appendTo($holder);
 	},
 
-	addDiffLinksTo: function($holder, rcids, diffs)
-	{
-		var $list = jQuery("<ul/>");
-		jQuery.each(diffs, function(i, diff){
-			$list.append( BunchPatroller.getDiffLink(rcids[i], diff) );
+	addDiffLinksTo: function($holder, revids) {
+		var $list = $("<ul/>");
+		$.each(revids, function(i, revid){
+			$list.append(BulkPatroller.getDiffLink(revid));
 		});
 		$list.appendTo($holder);
 	},
 
-	addShowAllDiffsLinkTo: function($holder)
-	{
-		jQuery('<a href="#alldiffs"/>')
+	addShowAllDiffsLinkTo: function($holder) {
+		$('<a href="#alldiffs"/>')
 			.text( gLang.msg("showalldiffs") )
 			.click(function(){
-				$all = jQuery('<div id="alldiffs"/>').insertAfter( $holder.parents(".diff") );
+				$all = $('<div id="alldiffs"/>').insertAfter( $holder.parents(".diff") );
 
-				jQuery("." + BunchPatroller.diffLinkClass, $holder).each(function(){
-					BunchPatroller.loadDiffContentFor(this, $all);
+				$("." + BulkPatroller.diffLinkClass, $holder).each(function(){
+					BulkPatroller.loadDiffContentFor(this, $all);
 				});
 
-				jQuery(this).remove();
+				$(this).remove();
 			})
 			.appendTo($holder);
 	},
 
-	loadDiffContentFor: function(link, $holder)
-	{
-		var $out = jQuery("<div/>").appendTo($holder).addClass(this.classLoading).before("<hr/>");
+	loadDiffContentFor: function(link, $holder) {
+		var $out = $("<div/>").appendTo($holder).addClass(this.classLoading).before("<hr/>");
 
-		jQuery.get(link.href + "&diffonly=1&action=render", function(data){
-			$out.html(data).removeClass( BunchPatroller.classLoading );
+		$.get(link.href + "&diffonly=1&action=render", function(data){
+			$out.html(data).removeClass(BulkPatroller.classLoading);
 		});
 	},
 
-	getDiffLink: function(rcid, diff)
-	{
+	getDiffLink: function(revid) {
 		return '<li><a'
-			+ ' id="' + this.getDiffLinkId(rcid) + '"'
+			+ ' id="' + this.getDiffLinkId(revid) + '"'
 			+ ' class="' + this.diffLinkClass + '"'
-			+ ' href="' + wgScript + '?oldid=prev&diff=' + diff + '&rcid=' + rcid + '"'
-			+ '>' + diff +'</a></li>';
+			+ ' href="' + mw.config.get('wgScript') + '?oldid=prev&diff=' + revid + '"'
+			+ '>' + revid +'</a></li>';
 	},
 
-	getDiffLinkId: function(rcid)
-	{
-		return "diff-link-" + rcid;
+	getDiffLinkId: function(revid) {
+		return "diff-link-" + revid;
 	},
 
-	executePatrol: function(rcids, motherLink)
-	{
-		var token = this.getToken();
-
-		jQuery.each(rcids, function(i, rcid){
-			BunchPatroller.executePatrolOne(token, rcid);
+	executePatrol: function(revids, motherLink) {
+		var api = new mw.Api();
+		api.getToken("patrol").done(function(token) {
+			$.each(revids, function(i, revid) {
+				BulkPatroller.executePatrolOne(token, revid);
+			});
+	
+			BulkPatroller.executeOnPatrolDone(function() {
+				$(motherLink).replaceWith(gLang.msg("markedaspatrolledtext"));
+				QuickPatroller.gotoRcIfWanted();
+			}, revids);
 		});
-
-		this.executeOnPatrolDone(function() {
-			if ( BunchPatroller.checkForBadToken() ) {
-				BunchPatroller.restartPatrol(rcids, motherLink);
-				return;
-			}
-			jQuery(motherLink).replaceWith( gLang.msg("markedaspatrolledtext") );
-			QuickPattroler.gotoRcIfWanted();
-		}, rcids);
-	},
-
-	restartPatrol: function(rcids, motherLink)
-	{
-		this.errors = [];
-		this.numEditsPatrolled = 0;
-		jQuery("." + this.diffLinkClass).removeClass(BunchPatroller.diffLinkClassNotDone);
-		BunchPatroller.clearToken().executePatrol(rcids, motherLink);
 	},
 
 	intervalId: 0,
-	executeOnPatrolDone: function(callback, rcids)
-	{
-		this.intervalId = setInterval(function(){
-			if ( BunchPatroller.numEditsPatrolled >= rcids.length ) {
-				clearInterval(BunchPatroller.intervalId);
+	executeOnPatrolDone: function(callback, revids) {
+		this.intervalId = setInterval(function() {
+			if (BulkPatroller.numEditsPatrolled >= revids.length) {
+				clearInterval(BulkPatroller.intervalId);
 				callback();
 			}
 		}, 200);
 	},
 
-	executePatrolOne: function(token, rcid)
-	{
-		var $diffLink = jQuery("#" + BunchPatroller.getDiffLinkId(rcid));
-		$diffLink.addClass( QuickPattroler.linkClassWorking );
-		jQuery.post(BunchPatroller.apiPath, {
-			action : "patrol",
-			token  : token,
-			rcid   : rcid,
-			format : "json"
-		}, function(data){
-			$diffLink.removeClass( QuickPattroler.linkClassWorking );
-			if ( data.error ) {
-				$diffLink.addClass(BunchPatroller.diffLinkClassNotDone);
-				BunchPatroller.handleError(data.error, $diffLink);
-			} else {
-				$diffLink.addClass(BunchPatroller.diffLinkClassDone);
-			}
-			BunchPatroller.numEditsPatrolled++;
-		}, "json");
+	executePatrolOne: function(token, revid) {
+		var $diffLink = $("#" + BulkPatroller.getDiffLinkId(revid));
+		$diffLink.addClass(QuickPatroller.linkClassWorking);
+		var api = new mw.Api();
+		api.post({
+			action: "patrol",
+			token: token,
+			revid: revid
+		}).done(function(data) {
+			$diffLink.removeClass(QuickPatroller.linkClassWorking).addClass(BulkPatroller.diffLinkClassDone);
+		}).fail(function() {
+			$diffLink.addClass(BulkPatroller.diffLinkClassNotDone);
+			BulkPatroller.handleError(data.error, $diffLink);
+		}).always(function() {
+			BulkPatroller.numEditsPatrolled++;
+		});
 	},
 
-	handleError: function(error, $diffLink)
-	{
+	handleError: function(error, $diffLink) {
 		this.errors.push(error.code);
 		$diffLink.attr("title", error.info);
-	},
-
-	checkForBadToken: function()
-	{
-		return $.inArray("badtoken", this.errors) !== -1;
-	},
-
-	tokenCookie: "patrolToken",
-
-	getToken: function()
-	{
-		var token = $.cookie(this.tokenCookie);
-		if ( ! token ) {
-			token = this.getTokenFromApi();
-			$.cookie(this.tokenCookie, token);
-		}
-
-		return token;
-	},
-
-	getTokenFromApi: function()
-	{
-		var token = null;
-		jQuery.ajax({
-			type:     "GET",
-			url:       this.apiPath + "?action=query&list=recentchanges"
-						+ "&rctoken=patrol&rclimit=1&format=json",
-			async:     false,
-			dataType: "json",
-			success:  function(data){
-				token = data.query.recentchanges[0].patroltoken;
-			}
-		});
-		return token;
-	},
-
-	clearToken: function()
-	{
-		$.cookie(this.tokenCookie, null);
-		return this;
 	}
 
 };
 
-// prepare for fight
 mw.hook('wikipage.content').add(function(){
-	if ( /^(Recentchanges|Watchlist)/.test(mw.config.get('wgCanonicalSpecialPageName')) ) {
-		BunchPatroller.makeBunchDiffsPatrollable();
-	} else if ( mw.config.get('wgAction') === "view" ) {
-		QuickPattroler.enable();
-		BunchPatroller.enable();
+	if ( /^(Recentchanges|Recentchangeslinked|Watchlist)/.test(mw.config.get('wgCanonicalSpecialPageName')) ) {
+		BulkPatroller.makeBulkDiffsPatrollable();
+	} else if (mw.config.get('wgAction') === "view") {
+		QuickPatroller.enable();
+		BulkPatroller.enable();
 	}
 });
