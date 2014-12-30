7be8c0706a749aa9e00bc9f68345fea29ff429a2
Borislav <b.manolov@gmail.com>
2014-12-30 15:55:20 +0000

    изтриване на "Technical restrictions" title fix, може да се замести напълно чрез вълшебната дума DISPLAYTITLE

--- Common.js
+++ Common.js
@@ -20,10 +20,6 @@ mw.ns = mw.ns || {
 	CATEGORY_TALK  : 15
 };
 
-var
-DEFAULT_USER_LANGUAGE = "bg",
-FALLBACK_USER_LANGUAGE = "en";
-
 mw.ext = mw.ext || {};
 
 /**
@@ -512,101 +508,6 @@ mw.loader.using(["mediawiki.cookie"]).done(function() {
 	$(".showme").show();
 	Memory.executeCurrentAction();
 });
-
-/* * * * * * * * * *   Title fix  * * * * * * * * */
-/*
-  Taken from [[en:MediaWiki:Common.js]]
-*/
-
-/** "Technical restrictions" title fix *****************************************
- *
- *  Description: For pages that have something like Template:Wrongtitle, replace
- *               the title, but only if it is cut-and-pasteable as a valid
- *               wikilink. For instance, "NZR WB class" can be changed to
- *               "NZR W<sup>B</sup> class", but [[C#]] is not an equivalent wikilink,
- *               so [[C Sharp]] doesn't have its main title changed.
- *
- *               The function looks for a banner like this:
- *               <div id="RealTitleBanner"> ... <span id="RealTitle">title</span> ... </div>
- *               An element with id=DisableRealTitle disables the function.
- *  Maintainers: Remember_the_dot
- */
-
-if (mw.config.get('wgIsArticle')) //prevents the "Editing " prefix from disappearing during preview
-{
-    mw.hook('wikipage.content').add(function()
-    {
-        var realTitle = document.getElementById("RealTitle")
-
-        if (realTitle)
-        {
-            //normalizes a title or a namespace name (but not both)
-            //trims leading and trailing underscores and converts (possibly multiple) spaces and underscores to single underscores
-            function normalizeTitle(title)
-            {
-                return title.replace(/^_+/, "").replace(/_+$/, "").replace(/[\s_]+/g, "_")
-            }
-
-            if (realTitle.textContent) //everyone but IE
-            {
-                var realTitleText = realTitle.textContent
-            }
-            else //IE
-            {
-                var realTitleText = realTitle.innerText
-            }
-
-            var normalizedRealTitle
-            var normalizedPageTitle
-            var indexOfColon = realTitleText.indexOf(":")
-            var normalizedNamespaceName = normalizeTitle(realTitleText.substring(0, indexOfColon)).toLowerCase()
-
-            //make namespace prefix lowercase and uppercase the first letter of the title
-            if (indexOfColon == -1 || mw.config.get('wgCanonicalNamespace').toLowerCase() != normalizedNamespaceName) //no namespace prefix - either no colon or a nonsensical namespace prefix (for example, "Foo" in "Foo: The Story of My Life")
-            {
-                normalizedRealTitle = normalizeTitle(realTitleText)
-                normalizedRealTitle = normalizedRealTitle.charAt(0).toUpperCase() + normalizedRealTitle.substring(1)
-                normalizedPageTitle = mw.config.get('wgPageName').charAt(0).toUpperCase() + mw.config.get('wgPageName').substring(1)
-            }
-            else //using a namespace prefix
-            {
-                var normalizedRealPageTitle = normalizeTitle(realTitleText.substring(indexOfColon + 1))
-
-                normalizedRealTitle = normalizedNamespaceName
-                if (normalizedNamespaceName != "") //namespace 0 is a special case where the leading colon should never be shown
-                {
-                    normalizedRealTitle += ":"
-                }
-                normalizedRealTitle += normalizedRealPageTitle.charAt(0).toUpperCase() + normalizedRealPageTitle.substring(1)
-                normalizedPageTitle = mw.config.get('wgPageName').substring(0, mw.config.get('wgPageName').indexOf(":") + 1).toLowerCase() + mw.config.get('wgPageName').substring(mw.config.get('wgPageName').indexOf(":") + 1)
-            }
-
-            if (normalizedRealTitle == normalizedPageTitle) //normalized titles match, so we can do full replacement
-            {
-                var h1 = document.getElementsByTagName("h1")[0]
-
-                //remove all child nodes, including text
-                while (h1.firstChild)
-                {
-                    h1.removeChild(h1.firstChild)
-                }
-
-                //populate with nodes of real title
-                while (realTitle.firstChild) //the children are moved to a new parent element
-                {
-                    h1.appendChild(realTitle.firstChild)
-                }
-
-                //delete the real title banner since the problem is solved
-                var realTitleBanner = document.getElementById("RealTitleBanner")
-                realTitleBanner.parentNode.removeChild(realTitleBanner)
-            }
-
-            //no matter what, correct the page title
-            document.title = realTitleText + " - Wikipedia, the free encyclopedia"
-        }
-    })
-}
 
 // Използвайте този код, за да пренасочите връзката от логото на Уикипедия
 // към определена страница в енциклопедията. За целта разкоментирайте кода
