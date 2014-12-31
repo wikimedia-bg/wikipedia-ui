8175dc0ce43bd667c5fdb97fab41379ab8f2c82b
Borislav <b.manolov@gmail.com>
2014-12-31 12:20:23 +0000

    load gadget from mediawiki.org

--- Gadget-edittop.js
+++ Gadget-edittop.js
@@ -1,1 +1,1 @@
-importOuterScript("MediaWiki:Gadget-edittop.js", "en");
+mw.loader.load('//www.mediawiki.org/w/index.php?title=MediaWiki:Gadget-edittop.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400');
