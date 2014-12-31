5cb1a61b298148d379a0fb73bfb2b5cd0d4d5245
Borislav <b.manolov@gmail.com>
2014-12-31 12:16:56 +0000

    load gadget from mediawiki.org

--- Gadget-UTCLiveClock.js
+++ Gadget-UTCLiveClock.js
@@ -1,1 +1,1 @@
-importOuterScript("MediaWiki:Gadget-UTCLiveClock.js", "en");
+mw.loader.load('//www.mediawiki.org/w/index.php?title=MediaWiki:Gadget-UTCLiveClock.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400');
