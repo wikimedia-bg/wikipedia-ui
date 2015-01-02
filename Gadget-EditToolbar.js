a1efc8e131e0df6c24b96ddccc8b9de3a761d9e1
Borislav <b.manolov@gmail.com>
2015-01-02 17:39:23 +0000

    define fake API as fallback when core is not loaded

--- Gadget-EditToolbar.js
+++ Gadget-EditToolbar.js
@@ -1,3 +1,14 @@
+( function(w) {
+	// define fake API as fallback when core is not loaded
+	w.setCustomInsButton =
+	w.setCustomMiscButton =
+	w.rmCustomInsButtons =
+	w.rmCustomMiscButtons =
+	w.rmCustomButtons =
+	w.putToolbar = function() {};
+	w.customInsButtons =
+	w.customMiscButtons = [];
+}(window) );
 if ($.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1) {
 	mw.loader.load('ext.gadget.EditToolbar-core');
 }
