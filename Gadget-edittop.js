d8fa09b83828015cbe7151c3848b76528a7078a4
Termininja <>
2015-08-09 08:19:39 +0000

    smaxage и maxage вече нямат ефект [[mw:Topic:Rvgndwt1lwhsl98o]]

--- Gadget-edittop.js
+++ Gadget-edittop.js
@@ -1,1 +1,1 @@
-mw.loader.load('//www.mediawiki.org/w/index.php?title=MediaWiki:Gadget-edittop.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400');
+mw.loader.load('//www.mediawiki.org/w/index.php?title=MediaWiki:Gadget-edittop.js&action=raw&ctype=text/javascript');
