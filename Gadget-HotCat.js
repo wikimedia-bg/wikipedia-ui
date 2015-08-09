5a3284963189cfe21424f0891fca1c78a8413a1a
Termininja <>
2015-08-09 09:08:29 +0000

    без smaxage и maxage

--- Gadget-HotCat.js
+++ Gadget-HotCat.js
@@ -5,4 +5,4 @@ window.hotcat_translations_from_commons = false;
  HotCat is a gadget to make changes to categories much easier.
  Full documentation can be found at [[commons:Help:Gadget-HotCat]]
 */
-mw.loader.load( '//commons.wikimedia.org/w/index.php?title=MediaWiki:Gadget-HotCat.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400' );
+mw.loader.load('//commons.wikimedia.org/w/index.php?title=MediaWiki:Gadget-HotCat.js&action=raw&ctype=text/javascript');
