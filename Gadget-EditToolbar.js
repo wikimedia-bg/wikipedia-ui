b95e5483446a874981b0c0d6bcf4751fa3f58670
V111P <>
2015-10-20 07:04:52 +0000

    поне _добавянето_ на бутони да работи и преди EditToolbar-core да се зареди

--- Gadget-EditToolbar.js
+++ Gadget-EditToolbar.js
@@ -1,14 +1,27 @@
 ( function(w) {
+	var customInsButtons = {};
+	var customMiscButtons = {};
+
+	w.setCustomInsButton = function(code, left, middle, right, shownText, title) {
+		customInsButtons[code] = [left, middle, right, shownText, title];
+	};
+
+	w.setCustomMiscButton = function(code, codeToRun, shownText, title) {
+		customMiscButtons[code] = [codeToRun, shownText, title];
+	};
+
 	// define fake API as fallback when core is not loaded
-	w.setCustomInsButton =
-	w.setCustomMiscButton =
 	w.rmCustomInsButtons =
 	w.rmCustomMiscButtons =
 	w.rmCustomButtons =
 	w.putToolbar = function() {};
 	w.customInsButtons =
-	w.customMiscButtons = [];
+	w.customMiscButtons = {};
 }(window) );
+
 if ($.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1) {
-	mw.loader.load('ext.gadget.EditToolbar-core');
+	mw.loader.using('ext.gadget.EditToolbar-core', function () {
+		$.extend(window.customInsButtons, customInsButtons);
+		$.extend(window.customMiscButtons, customMiscButtons);
+	});
 }
