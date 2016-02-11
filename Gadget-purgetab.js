cc24f3a65681fd73874a5be5553117aaacee80c8
He7d3r <>
2016-02-11 11:38:39 +0000

    [[mw:RL/MGU|Migration]]: addOnloadHook → $; addPortletLink → mw.util.addPortletLink

--- Gadget-purgetab.js
+++ Gadget-purgetab.js
@@ -1,9 +1,9 @@
 /* Purge caption button */
-addOnloadHook(function () {
+$(function () {
    var hist; var url;
    if (!(hist = document.getElementById('ca-history') )) return;
    if (!(url = hist.getElementsByTagName('a')[0] )) return;
    if (!(url = url.href )) return;
-   addPortletLink('p-cactions', url.replace(/([?&]action=)history([&#]|$)/, '$1purge$2'),
+   mw.util.addPortletLink('p-cactions', url.replace(/([?&]action=)history([&#]|$)/, '$1purge$2'),
                   '*', 'ca-purge', 'Изчистване на складираното копие на страницата', '*');
 });
