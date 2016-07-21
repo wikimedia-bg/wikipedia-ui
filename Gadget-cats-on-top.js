e29516e37dd6d763cf89fcedd2e5233f4ad02ea4
Krenair <>
2016-07-21 02:51:16 +0000

    Maintenance: [[mw:RL/MGU]] / [[mw:RL/JD]] - addOnloadHook is deprecated

--- Gadget-cats-on-top.js
+++ Gadget-cats-on-top.js
@@ -1,4 +1,4 @@
-addOnloadHook(function(){
+$(function(){
 	var cats = document.getElementById("catlinks");
 	if ( !cats ) return;
 	document.getElementById("contentSub").appendChild( cats.cloneNode(true) );
