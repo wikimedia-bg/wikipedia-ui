49b0cc43655a51ba75a5750d080901f5a1b62e09
V111P <>
2016-09-14 04:08:23 +0000

    да не се чупи когато има грешки в други скриптове

--- Gadget-EditToolbar-core.js
+++ Gadget-EditToolbar-core.js
@@ -296,11 +296,19 @@ function setupCustomEditTools() {
 	});
 }
 
-$(function() {
+function entryPoint() {
 	mw.loader.using("user", function(){
 		setupCustomEditTools();
 	});
-});
+}
+
+// not to depend on $() so it doesn't break when there are errors in other scripts
+if (document.readyState == 'complete')
+	entryPoint();
+else if (window.addEventListener)
+	window.addEventListener('load', entryPoint, false);
+else
+	window.attachEvent('onload', entryPoint);
 
 function appendCustomButtons(parent) {
 	var buts = $('<div>', { id: "custombuttons" });
