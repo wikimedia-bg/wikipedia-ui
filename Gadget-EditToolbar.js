715a36d1cfa39f5ae56f46c9d45787ed131c57df
Borislav <b.manolov@gmail.com>
2016-09-17 12:23:44 +0000

    опростяване

--- Gadget-EditToolbar.js
+++ Gadget-EditToolbar.js
@@ -1,8 +1,6 @@
-( function(w) {
-	// define fake API as fallback when core is not loaded
-	w.putToolbar = function() {};
+// define fake API as fallback when core is not loaded
+window.putToolbar = function() {};
 
-	if ($.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1) {
-		mw.loader.load('ext.gadget.EditToolbar-core');
-	}
-}(window) );
+if ($.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1) {
+	mw.loader.load('ext.gadget.EditToolbar-core');
+}
