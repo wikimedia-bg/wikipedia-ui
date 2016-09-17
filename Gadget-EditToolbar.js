df277a849af30313abe6fe90e112d983042229c0
Borislav <b.manolov@gmail.com>
2016-09-17 12:22:14 +0000

    аварийните функции вече не са нужни

--- Gadget-EditToolbar.js
+++ Gadget-EditToolbar.js
@@ -1,28 +1,8 @@
 ( function(w) {
-	var customInsButtons = {};
-	var customMiscButtons = {};
-
-	w.setCustomInsButton = function(code, left, middle, right, shownText, title) {
-		customInsButtons[code] = [left, middle, right, shownText, title];
-	};
-
-	w.setCustomMiscButton = function(code, codeToRun, shownText, title) {
-		customMiscButtons[code] = [codeToRun, shownText, title];
-	};
-
 	// define fake API as fallback when core is not loaded
-	w.rmCustomInsButtons =
-	w.rmCustomMiscButtons =
-	w.rmCustomButtons =
 	w.putToolbar = function() {};
-	w.customInsButtons =
-	w.customMiscButtons = {};
 
 	if ($.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1) {
-		mw.loader.using('ext.gadget.EditToolbar-core', function () {
-			$.extend(w.customInsButtons, customInsButtons);
-			$.extend(w.customMiscButtons, customMiscButtons);
-		});
+		mw.loader.load('ext.gadget.EditToolbar-core');
 	}
-
 }(window) );
