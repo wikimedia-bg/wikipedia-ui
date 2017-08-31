50f97ac6f148694e58ab8202bb5d8b2554cb1dda
Luchesar ILIEV <luchesar.iliev@gmail.com>
2017-08-31 21:49:33 +0000

    Премахната редакция 8053161 на [[Special:Contributions/Iliev|Iliev]] ([[User talk:Iliev|б.]]) д___ и кеширането

--- Gadget-EditToolbar-core.js
+++ Gadget-EditToolbar-core.js
@@ -4,6 +4,8 @@
  * Some functions and arrays are still bound to window for backwards compatibility
  */
 
+mw.loader.using('/w/index.php?title=MediaWiki:Common.js&action=raw&ctype=text/javascript').then( function() {
+
 /**
  * insert an edit toolbar before the textarea.
  */
@@ -842,3 +844,5 @@ function zamena_5ko() {
 		txt = txt.replace(r, a_petko[i++]);
 	}
 }
+
+})
