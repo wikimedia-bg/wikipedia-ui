1969b5afd3676e560f11c5eb5b24cba1641d11c7
Nk <>
2018-11-07 13:20:06 +0000

    от фр:

---
+++ Gadget-MonobookToolbar.js
@@ -0,0 +1,117 @@
+
+if(typeof(MonobookToolbar)=="undefined"){  // Test anti-double inclusion
+
+var MonobookToolbar = {};
+window.MonobookToolbar = MonobookToolbar;
+
+MonobookToolbar.buttons = [];
+
+MonobookToolbar.functions = {};
+
+MonobookToolbar.toolbarId = mw.user.options.get('usebetatoolbar') ? 'monobooktoolbar' : 'toolbar';
+
+MonobookToolbar.functions.Init = function($){
+   var Textarea = document.getElementById("wpTextbox1");
+   if(!Textarea) return;
+   var UseBetaToolbar = mw.user.options.get('usebetatoolbar');
+   var forceOldToolbar = mw.user.options.get('gadget-ForceMonobookToolbar');
+   if(UseBetaToolbar && !forceOldToolbar) return;
+
+   var toolbar = document.getElementById(MonobookToolbar.toolbarId);
+   if(!toolbar){
+      toolbar = document.createElement('div');
+      toolbar.id = MonobookToolbar.toolbarId;
+      Textarea.parentNode.insertBefore(toolbar, Textarea);
+   }
+   MonobookToolbar.functions.InitButtons();
+};
+
+MonobookToolbar.functions.InitButtons = function(){
+    for(var a=0,l=MonobookToolbar.buttons.length;a<l;a++){
+        MonobookToolbar.functions.InsertButton(a);
+    }
+};
+
+MonobookToolbar.functions.InsertButton = function(index){
+    var parent = document.getElementById(MonobookToolbar.toolbarId);
+    var item = MonobookToolbar.buttons[index];
+    if(!parent || !item) return false;
+    if(item.imageId) {
+      var oldImage = document.getElementById(item.imageId);
+      if (oldImage) {
+        oldImage.parentNode.removeChild(oldImage);
+      }
+    }
+    var image = document.createElement("img");
+    image.width = 23;
+    image.height = 22;
+    if (item.imageId) image.id = item.imageId;
+    image.src = item.imageFile;
+    image.border = 0;
+    image.alt = item.speedTip;
+    image.className = "mw-toolbar-editbutton";
+    image.title = item.speedTip;
+    image.onclick = function() {
+        MonobookToolbar.functions.insertTags(item.tagOpen, item.tagClose, item.sampleText);
+        return false;
+    };
+    parent.appendChild(image);
+    return true;
+};
+
+MonobookToolbar.functions.insertTags = function(tagOpen, tagClose, sampleText){
+    var txtarea = document.getElementById("wpTextbox1");
+    var selText, isSample = false;
+
+    //save textarea scroll position
+    var textScroll = txtarea.scrollTop;
+    //get current selection
+    txtarea.focus();
+    var startPos = txtarea.selectionStart;
+    var endPos = txtarea.selectionEnd;
+    selText = txtarea.value.substring(startPos, endPos);
+    //insert tags
+    checkSelectedText();
+    txtarea.value = txtarea.value.substring(0, startPos) + tagOpen + selText + tagClose + txtarea.value.substring(endPos, txtarea.value.length);
+    //set new selection
+    if (isSample) {
+        txtarea.selectionStart = startPos + tagOpen.length;
+        txtarea.selectionEnd = startPos + tagOpen.length + selText.length;
+    } else {
+        txtarea.selectionStart = startPos + tagOpen.length + selText.length + tagClose.length;
+        txtarea.selectionEnd = txtarea.selectionStart;
+    }
+    //restore textarea scroll position
+    txtarea.scrollTop = textScroll;
+
+    function checkSelectedText(){
+        if (!selText) {
+            selText = sampleText;
+            isSample = true;
+        } else if (selText.charAt(selText.length - 1) == ' ') { //exclude ending space char
+            selText = selText.substring(0, selText.length - 1);
+            tagClose += ' ';
+        }
+    }
+};
+
+MonobookToolbar.functions.CreateButton = function(imageFile, speedTip, tagOpen, tagClose, sampleText, imageId){
+    var NewIndex = MonobookToolbar.buttons.length;
+    MonobookToolbar.buttons[NewIndex] = {
+        "imageId": imageId,
+        "imageFile": imageFile,
+        "speedTip": speedTip,
+        "tagOpen": tagOpen,
+        "tagClose": tagClose,
+        "sampleText": sampleText
+    };
+    if(document.getElementById(MonobookToolbar.toolbarId)){
+        if(imageId){
+            MonobookToolbar.functions.InsertButton(NewIndex);
+        }
+    }
+};
+
+$(MonobookToolbar.functions.Init);
+
+}  // Fin test anti-double inclusion
