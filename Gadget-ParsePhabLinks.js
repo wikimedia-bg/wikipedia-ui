17c052195339241f5509520c9a1c714443e89d14
Luchesar ILIEV <luchesar.iliev@gmail.com>
2018-11-15 13:51:56 +0200

    Parse the Phabricator links on other relevant pages as well
    Summary:
    D5 implemented parsing the links to Phabricator in JSS 9's summaries only when viewing the history of pages in the MediaWiki and Module namespaces. However, these links appear also in other places, where it is still desirable to convert them to proper hyperlinks:
    - Special:Contributions/JSS 9,
    - Special:Recentchanges,
    - Special:Watchlist,
    - diff links of pages in NS MediaWiki and Module.
    
    Test Plan: The code behaves as expected in the browser's JS console.
    
    Reviewers: Borislav
    
    Reviewed By: Borislav
    
    Tags: #js_css_user_interface
    
    Differential Revision: https://phabricator.wikimedia.bg/D6

--- Gadget-ParsePhabLinks.js
+++ Gadget-ParsePhabLinks.js
@@ -1,28 +1,63 @@
-( function( $, mw, undefined ) {
-	if (
-		mw.config.get( 'wgAction' ) !== 'history' ||
-		$.inArray( mw.config.get( 'wgCanonicalNamespace' ), [ 'MediaWiki', 'Module' ] ) === -1
-	) return;
-	$( function() {
-		$.each( $( 'ul#pagehistory > li' ), function() {
-			if ( $( this ).find( 'a.mw-userlink' ).text() === 'JSS 9' ) {
-				var $comment = $( this ).find( 'span.comment' );
-				if ( $comment.html() ) {
-					var commentOldHtml = $comment.html().split( '|' );
-					// commentOldHtml[2] includes the span.comment closing parenthesis,
-					// hence why we discard it by slice() and re-add it after </a>.
-					$comment.html(
-						$.trim( commentOldHtml[0] ) +
-						': <a href="' +
-						$.trim( commentOldHtml[1] ) +
-						'" target="_blank">' +
-						$.trim( commentOldHtml[2] ).slice( 0, -1 ) +
-						'</a>)'
-					);
-				}
+function parsePhabLinks() {
+	var $jss9Comments =
+	// The method of selecting only and all JSS 9's comments depends on the page we're on.
+		( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Contributions' ) ?
+	// If this is the page with JSS 9's contributions, simply select all span.comments on it.
+			$( 'span.comment' ) :
+	// Elsewhere first get the <a> elements that link to JSS 9's user page.  They are part of the
+	// rev or diff we need. To get the comment itself, go up two nodes and find a span.comment
+	// descendant (there should be only one).  The type of those parent elements varies with the
+	// page we are processing, but going up two nodes works universally, which is why we use the
+	// somewhat clumsy double parent(), instead of e.g. closest().
+			$( 'a.mw-userlink' )
+				.filter( function() { return $( this ).text() === 'JSS 9'; } )
+				.parent().parent().find( 'span.comment' );
+	// Process each of the selected span.comment elements.
+	$.each( $jss9Comments, function() {
+			if ( $( this ).html() ) {
+				var oldHtml = $( this ).html().split( '|' );
+				// oldHtml[2] includes the span.comment closing parenthesis, which we do not want to
+				// include in the link itself. Discard it by slice() and re-add it after the </a>.
+				$( this ).html(
+					$.trim( oldHtml[0] ) +
+					': <a href="' +
+					$.trim( oldHtml[1] ) +
+					'" target="_blank">' +
+					$.trim( oldHtml[2] ).slice( 0, -1 ) +
+					'</a>)'
+				);
 			}
-		} );
-	} );
+		}
+	);
+}
+
+( function( $, mw, undefined ) {
+	// Parse the links only if...
+	if (
+		// ...this is Special:Recentchanges or Special:Watchlist...
+		$.inArray(
+			mw.config.get( 'wgCanonicalSpecialPageName' ),
+			[ 'Recentchanges', 'Watchlist' ]
+		) !== -1 ||
+		// ...OR if this is Special:Contributions/JSS 9...
+		(
+			mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Contributions' &&
+			mw.config.get( 'wgRelevantUserName' ) === 'JSS 9'
+		) ||
+		// ...OR if this is...
+		(
+			// ...a page in MediaWiki or Module namespace...
+			$.inArray(
+				mw.config.get( 'wgCanonicalNamespace' ),
+				[ 'MediaWiki', 'Module' ]
+			) !== -1 &&
+			// ...AND we are looking at its history or a diff link.
+			(
+				mw.config.get( 'wgDiffOldId' ) !== null ||
+				mw.config.get( 'wgAction' ) === 'history'
+			)
+		)
+	) $( parsePhabLinks() );
 } )( jQuery, mediaWiki );
 
-// vim: ts=4 sts=4 sw=4 noet:
+// vim: ts=4 sts=4 sw=4 tw=100 noet:
