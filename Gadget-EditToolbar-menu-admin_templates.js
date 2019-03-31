deaa377a429f795c853e719a5694a4a3de4a7659
Luchesar ILIEV <luchesar.iliev@gmail.com>
2019-03-31 13:26:33 +0300

    [EditToolbar] + moar admin templates

--- Gadget-EditToolbar-menu-admin_templates.js
+++ Gadget-EditToolbar-menu-admin_templates.js
@@ -1,12 +1,22 @@
 window.atpl.push( {
 	// "SHOWN TEXT" : "PAGE NAME",
 	"Шаблони за администратори…" : "-",
-	"П-блок1" : window.atplBasePath + "П-блок1",
-	"П-блок птв" : window.atplBasePath + "П-блок птв",
-	"Други цели" : window.atplBasePath + "Други цели",
-	"Неприемливо име" : window.atplBasePath + "Неприемливо име",
-	"Групова/фирмена" : window.atplBasePath + "Групова/фирмена",
-	"Марионетка" : window.atplBasePath + "Марионетка",
-	"Безсрочно други" : window.atplBasePath + "Безсрочно други"
+	"За беседи" : {
+		"П-блок1 (без посочена причина)" : window.atplBasePath + "П-блок1",
+		"П-блок1 (злоупотр. с прив. за ред.)" : window.atplBasePath + "П-блок1ванд",
+		"П-блок1 (заобикаляне на пр. блок.)" : window.atplBasePath + "П-блок1блок",
+		"П-блок1 (агресивно поведение, НЛН)" : window.atplBasePath + "П-блок1нлн",
+		"П-блок1 (злоупотреба с марионетки)" : window.atplBasePath + "П-блок1зсм",
+		"П-блок1 (спам във външн. препратки)" : window.atplBasePath + "П-блок1внп",
+		"П-блок1 (създ. на У не е осн. цел)" : window.atplBasePath + "П-блок1цели",
+		"П-блок птв (повече от три връщания)" : window.atplBasePath + "П-блок птв"
+	},
+	"За потребителски страници" : {
+		"Други цели" : window.atplBasePath + "Други цели",
+		"Неприемливо име" : window.atplBasePath + "Неприемливо име",
+		"Групова/фирмена" : window.atplBasePath + "Групова/фирмена",
+		"Марионетка" : window.atplBasePath + "Марионетка",
+		"Безсрочно други" : window.atplBasePath + "Безсрочно други"
+	}
 } );
 // vim: set ts=8 sts=8 sw=8 noet:
