4b165eec285e1ce6023ce54c9b157cc7c8ea71cd
Luchesar ILIEV <luchesar.iliev@gmail.com>
2020-02-05 20:16:46 +0200

    Меню -> преместване в инкубатор
    Summary: Добавя в менюто на статии възможност за бързо преместване в инкубатора.
    
    Test Plan: Тествано в моя потребителски `common.js`.
    
    Reviewers: Borislav, Сале, ShadeOfGrey, Scroch, Dido3
    
    Reviewed By: Borislav
    
    Subscribers: Ikonact, Мико, Spiritia, Спасимир
    
    Differential Revision: https://phabricator.wikimedia.bg/D10

---
+++ Gadget-MoveToIncubator.js
@@ -0,0 +1,59 @@
+function initMoveToIncubatorMenu() {
+
+	// =======================  CONFIGURATION  =======================
+
+	var incubatorPrefix = 'Уикипедия:Инкубатор/Статии/';
+
+	var menuText = '... в инкубатор';
+	var menuHotkey = 'z';
+	var menuPopupText = 'Преместване на статията в инкубатора';
+
+	var moveLeaveRedirect = '0'; // '0' - false, '1' - true
+	var moveReason = (
+		'Преместване в [[Уикипедия:Инкубатор|инкубатора]], ' +
+		'тъй като статията не отговаря на изискванията на Уикипедия.'
+	);
+
+	// ===============================================================
+
+	var scriptBase = mw.config.get( 'wgScript' ) + '?';
+
+	var movePageTarget = 'Special:MovePage/' + mw.config.get( 'wgPageName' );
+	var movePageNewTitle = incubatorPrefix + mw.config.get( 'wgTitle' );
+
+	var menuElementPrefix = '<li id="ca-move-incubator">';
+	var menuElementSuffix = '</li>';
+
+	var menuPopupFullText = menuPopupText + ' [alt-shift-' + menuHotkey + ']';
+
+	var menuHrefParams = [
+		'title=' + movePageTarget,
+		'wpNewTitle=' + movePageNewTitle,
+		'wpLeaveRedirect=' + moveLeaveRedirect,
+		'wpReason=' + moveReason
+	];
+
+	var menuHrefLink = 'href="' + encodeURI( scriptBase + menuHrefParams.join( '&' ) ) + '"';
+	var menuHrefTitle = 'title="' + menuPopupFullText + '"';
+	var menuHrefHotkey = 'accesskey="' + menuHotkey + '"';
+
+	var menuHrefAttribs = [
+		menuHrefLink,
+		menuHrefTitle,
+		menuHrefHotkey
+	];
+
+	var menuHrefHTML = '<a ' + menuHrefAttribs.join( ' ' ) + '>' + menuText + '</a>';
+
+	// Add the new menu option after the existing "Move" one.
+	$( '#ca-move' ).after( menuElementPrefix + menuHrefHTML + menuElementSuffix );
+
+}
+
+
+$( function () {
+	if ( mw.config.get( 'wgNamespaceNumber' ) === 0 ) initMoveToIncubatorMenu();
+} );
+
+
+// vim: ts=4 sts=4 sw=4 tw=100 noet:
