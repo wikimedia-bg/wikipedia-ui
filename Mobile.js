8c75fb79730357ef8799f9ac3a5f05f01f156f50
Jon (WMF) <>
2020-12-15 20:21:44 +0000

    maintenance: [[metawiki:User:Jon_(WMF)/Edit_to_user_or_gadget_script|more info]]   TypeError: mw.vars is undefined

---
+++ Mobile.js
@@ -0,0 +1,7 @@
+/* All JavaScript here will be loaded for users of the mobile site */
+mw.vars = {
+	use: function() {
+		// please translate
+		mw.notify('You are loading a gadget on mobile that is not supported. Please notify your gadget developer.')
+	}
+};
