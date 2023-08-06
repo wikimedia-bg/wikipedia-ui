2554f789ba80b6616a3987e8be64a93640f07b09
Ted Masters <>
2023-08-06 16:57:24 +0000

    *** празно резюме ***

--- Gadget-Quick patrol.js
+++ Gadget-Quick patrol.js
@@ -91,7 +91,7 @@ mw.ext.Patroller.bulk = function(quick) {
 	/** Works on Special:Recentchanges */
 	my.makeBulkDiffsPatrollable = function() {
 		$('table.mw-enhanced-rc').each(function() {
-			if ($(this).find('.unpatrolled').length) {
+			if ($(this).find('.unpatrolled')) {
 				my.makeBulkDiffPatrollable(this);
 			}
 		});
