60d9469a302f4191abb72b3f57a065d607731021
Nk <>
2023-12-07 14:58:45 +0000

    (почти) няма локални картинки

---
+++ Gadget-EditToolbar-menu-other templates.js
@@ -0,0 +1,14 @@
+window.atpl.push( {
+	"Други шаблони…" : "-",
+	"Шаблони за категории" : {
+		"Категория" : window.atplBasePath + "Категория",
+		"Категория инфо" : window.atplBasePath + "Категория инфо"
+	},
+	"Шаблони за шаблони" : {
+		"Навигационен шаблон" : window.atplBasePath + "Навигационен шаблон",
+		"Шаблон на мъниче" : window.atplBasePath + "Шаблон на мъниче",
+		"Includeonly" : window.atplBasePath + "Includeonly",
+		"Noinclude" : window.atplBasePath + "Noinclude"
+	}
+} );
+// vim: set ts=8 sts=8 sw=8 noet:
