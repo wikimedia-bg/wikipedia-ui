b8ffb5b6b6769088c428ff686291c6d9eaca4136
Sophivorus <>
2025-09-04 14:54:16 +0000

    Update init code

--- Gadget-ProveIt.js
+++ Gadget-ProveIt.js
@@ -1,15 +1,19 @@
 /**
  * ProveIt is a reference manager for Wikipedia and any other MediaWiki wiki
- * Documentation at https://www.mediawiki.org/wiki/ProveIt
- *
- * This initialization script sets the configuration options specific to this wiki
- * and then loads the ProveIt code directly from MediaWiki.org
+ * Documentation: https://www.mediawiki.org/wiki/ProveIt
+ * Source code: https://www.mediawiki.org/wiki/MediaWiki:Gadget-Global-ProveIt.js
  */
 function loadProveIt() {
-	mw.config.set({
-		'proveit-tag': 'Редакция с ProveIt', // Revision tag defined at Special:Tags (optional)
-		'proveit-summary': 'Източник редактиран с ProveIt', // Automatic edit summary (optional)
-		'proveit-templates': [ // Citation templates (without namespace)
+	mw.config.set( {
+
+		// Revision tag defined at Special:Tags (optional)
+		'proveit-tag': 'Редакция с ProveIt',
+
+		// Automatic edit summary (optional)
+		'proveit-summary': 'Източник редактиран с ProveIt',
+
+		// Local citation templates (without namespace)
+		'proveit-templates': [
 			'Cite book',
 			'Цитат книга',
 			'Cite web',
@@ -17,15 +21,15 @@ function loadProveIt() {
 			'Cite journal',
 			'Cite news'
 		],
-		'proveit-namespaces': [ // Supported namespaces (see https://www.mediawiki.org/wiki/Manual:Namespace_constants)
-			0, // Main namespace
-			2, // User namespace
-		]
-	});
-	mw.loader.load( '//www.mediawiki.org/w/load.php?modules=ext.gadget.ProveIt&only=scripts' );
-	mw.loader.load( '//www.mediawiki.org/w/load.php?modules=ext.gadget.ProveIt&only=styles', 'text/css' );
+
+		// Supported namespaces, see https://www.mediawiki.org/wiki/Manual:Namespace_constants
+		'proveit-namespaces': [ 0, 2 ],
+	} );
+
+	// Load from the central, global version at MediaWiki.org
+	mw.loader.load( '//www.mediawiki.org/w/load.php?modules=ext.gadget.Global-ProveIt' );
 }
 
 // Only load when editing
-mw.hook( 'wikipage.editform' ).add( loadProveIt );
-mw.hook( 've.activationComplete' ).add( loadProveIt );
+mw.hook( 'wikipage.editform' ).add( editForm => window.ProveIt || loadProveIt() );
+mw.hook( 've.newTarget' ).add( target => target.constructor.static.name === 'article' && target.on( 'surfaceReady', loadProveIt ) );
